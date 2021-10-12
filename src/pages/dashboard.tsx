ebd9a202324af2c921580e2a58fdbdb3e650632b
Luiz Spinelli <luiz.luchi@gmail.com>
2021-10-12 12:19:27 -0300

    charts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,63 @@
 import { Header } from "../components/Header";
-import { Flex } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
 import React from "react";
 import { Sidebar } from "../components/Sidebar";
+import dynamic from "next/dynamic";
+import { ApexOptions } from "apexcharts";
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2021-10-12",
+      "2021-10-13",
+      "2021-10-14",
+      "2021-10-15",
+      "2021-10-16",
+      "2021-10-17",
+      "2021-10-18",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [{ name: "series1", data: [31, 120, 45, 12, 53, 124, 100] }];
 
 export default function Dashboard() {
   return (
@@ -9,6 +65,25 @@ export default function Dashboard() {
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignItems="flex-start"
+        >
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da Semana
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
